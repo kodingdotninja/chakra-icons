5b17ff515e1a4721adc77c3e0383c2a1ed148c13
ri7nz <ri7nz@evilfactory.id>
2021-09-04 18:43:27 +0800

    feat: CLI support pipe and non-pipe
    * -i: it can be file or directories
    * -o: just single file for write the output
    * -n: name your displayName properties (only for single children or element in svg)

--- index.bin.js
+++ index.bin.js
@@ -1,63 +1,73 @@
 #!/usr/bin/env node
-/**
- * usage
- *
- * TODO: Pipeable in shell
- *
- * # Input
- * ```console
- * cat icon.svg | npx create-chakra-icons
- * ```
- * # Output
- * ```console
- * export const Icon = createIcon({
- *  displayName: "Icon",
- *  viewBox: "0 0 0 0",
- *  d: "M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0",
- * })
- * ```
- */
 const Fs = require("fs");
-// const Task = require("folktale/concurrency/task");
-// const Maybe = require("folktale/maybe");
-// const Result = require("folktale/result");
-const SvgParser = require("svg-parser");
-// const HastUtils = require("@svgr/hast-util-to-babel-ast").default;
+const Path = require("path");
 const BabelGenerator = require("@babel/generator").default;
 const argv = require("minimist")(process.argv.slice(2));
-const {
-  createChakraProperties,
-  createChakraFunctional,
-  createSourceChakraIcon,
-  hastSVGToChakraProperties,
-} = require("./lib");
+const { createChakraIcon } = require("./lib/chakra");
+const { stdout: output, stdin: input, exit } = process;
+const encoding = "utf-8";
+if (input.isTTY) {
+  main(argv);
+} else {
+  input.setEncoding(encoding);
+  input.on("data", function (data) {
+    if (data) {
+      const name = argv.name || argv.n || "Unamed";
+      const source = createCode(name, {
+        file: data,
+        displayName: name,
+      });
+      output.write(source);
+    }
+  });
+}
 
-main(argv);
-
+function createCode(name, ...svgs) {
+  const icon = createChakraIcon(name, ...svgs);
+  return BabelGenerator(icon).code;
+}
 // :: [Object] -> () *Effect*
 function main(args) {
-  const stdin = process.openStdin();
-  let data = "";
-  stdin.on("data", (chunk) => {
-    data += chunk;
-  });
+  const fileOrDirs = args.i || args.input;
+  const outFile = args.o || args.output;
+  const name = args.name || args.n || "Unamed";
 
-  stdin.on("end", () => {
-    const svgHast = SvgParser.parse(data);
-    const name = args.name || args.n || "<NamedHere>";
-
-    const chakraProperties = createChakraProperties({
-      displayName: name,
-      ...hastSVGToChakraProperties(svgHast),
-    });
-
-    const iconAst = createChakraFunctional({
-      displayName: name,
-      objectExpression: [chakraProperties],
-    });
-
-    const sourceAst = createSourceChakraIcon(iconAst);
-    const { code: source } = BabelGenerator(sourceAst);
-    process.stdout.write(source);
+  // check is {file} exist
+  Fs.exists(fileOrDirs, (isExist) => {
+    if (!isExist) {
+      console.error("Cannot handle input") && exit(1);
+    }
+    // all the input will put in an array {_files}
+    let _files = [];
+    // handle when {-i} || {--input} is file.svg or dirs/
+    if (Fs.lstatSync(fileOrDirs).isDirectory()) {
+      const pathResolved = Path.resolve(fileOrDirs);
+      _files = _files.concat(
+        Fs.readdirSync(pathResolved)
+          .filter((f) => f.split(".")[1] === "svg")
+          .map((f) => Path.join(pathResolved, f))
+      );
+    } else {
+      _files = _files.concat([fileOrDirs]);
+    }
+    // make code
+    const source = createCode(
+      name,
+      ..._files.map((_file) => ({
+        file: Fs.readFileSync(_file, encoding),
+        displayName: (() => {
+          const [_name] = Path.basename(_file).split(".") || [name];
+          return _name;
+        })(),
+      }))
+    );
+    // write output in output
+    outFile
+      ? Fs.writeFile(Path.resolve(outFile), source, (err) => {
+          if (err) {
+            console.log(err) && exit(1);
+          }
+        })
+      : output.write(source);
   });
 }
