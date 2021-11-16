981b2d3976c65cbec82e8526d0fca6dab77efa20
ri7nz <ri7nz@evilfactory.id>
2021-11-16 14:21:04 +0800

    fix(issue-7): missing validation for tag path
    Solve issue https://github.com/kodingdotninja/create-chakra-icons/issues/7

---
+++ examples/Issue7.js
@@ -0,0 +1,2 @@
+import { Icon } from "@chakra-ui/react";
+export const Unamed = props => <Icon viewBox="0 0 67 67" {...props}><g fill="none" stroke="#000"><path d="m18.951 33.437 9.95 9.951L48.36 23.929" /><circle cx={33.5} cy={33.5} r={33.5} stroke="none" /><circle cx={33.5} cy={33.5} r={33} /></g></Icon>;
