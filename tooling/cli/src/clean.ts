3afac4e30d721ce8b7ef2b9154261cb11d3d4236
RiN <ri7nz@evilfactory.id>
2022-01-29 14:41:47 +0700

    chore(tooling, cli, msrconfig): add clean and updates templates  (#23)
    * chore: rebuild lock-file and add clear command
    
    * feat(tooling/cli): add clean command
    
    * chore(templates): add clean in generated chakra-icons
    
    * chore(msrconfig): fix disable release for docs type commit

---
+++ tooling/cli/src/clean.ts
@@ -0,0 +1,24 @@
+import { BuildOptions } from "./types";
+
+import * as A from "fp-ts/Array";
+import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/function";
+import * as TE from "fp-ts/TaskEither";
+import * as fs from "fs/promises";
+import _glob from "glob";
+import path from "path";
+import { promisify } from "util";
+
+export const clean = (options: BuildOptions) =>
+  pipe(
+    TE.tryCatch(() => promisify(_glob)(path.join(options.sourcePath, "**", "*.{ts,tsx}")), E.toError),
+    TE.chainFirst(() => TE.tryCatch(() => fs.rmdir(options.clonePath, { recursive: true, maxRetries: 2 }), E.toError)),
+    TE.chainFirst(() => TE.tryCatch(() => fs.rmdir(path.join("dist"), { recursive: true, maxRetries: 2 }), E.toError)),
+    TE.chainFirst((files) =>
+      pipe(
+        files,
+        A.map((file) => TE.tryCatch(() => fs.rm(file), E.toError)),
+        TE.sequenceSeqArray,
+      ),
+    ),
+  );
