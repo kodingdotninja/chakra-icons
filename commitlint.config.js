71474d42f4f2387e04df7fe256143c14b4783f8a
ri7nz <ri7nz@evilfactory.id>
2022-01-29 11:50:11 +0700

    chore(commitlint): add ignore for squash or body-maximum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   extends: ["squash-pr", "@commitlint/config-conventional"],
-  ignores: [(msg) => /^chore\(release\): (.*) \[skip ci\]/g.test(msg)],
+  ignores: [
+    // ignore lint commit when committed by release commit
+    (msg) => /^chore\(release\): (.*) \[skip ci\]/g.test(msg),
+    // ignore lint commit when squash message
+    (msg) => /.*:.*\(#\d+\)/g.test(msg),
+  ],
 };
