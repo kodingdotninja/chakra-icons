a5b3a61b635239a9161ea36e5220ca3b362845f2
ri7nz <ri7nz@evilfactory.id>
2022-02-03 03:02:33 +0700

    chore(commitlint): update ignore rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["squash-pr", "@commitlint/config-conventional"],
   ignores: [
     // ignore lint commit when committed by release commit
-    (msg) => /^chore\(release\): (.*) \[skip ci\]/g.test(msg),
+    (msg) => /^chore\((release|deps-dev|deps)?\):\s(@?\D+\d+.\d+.\d+|bump.*)\s(from|\[skip ci\]|to.*)?/g.test(msg),
     // ignore lint commit when squash message
     (msg) => /.*:.*\(#\d+\)/g.test(msg),
   ],
