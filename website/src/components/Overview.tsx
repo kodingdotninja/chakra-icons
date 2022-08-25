c76086ef6057a001c20d13ce529d1cdacd11396a
RiN <ri7nz@evilfactory.id>
2022-08-25 15:39:31 +0800

    feat(website): change UI for copy and add copy as svg (#262)
    * feat(website): add cryptocurrencies component for overview
    
    * feat(website): add copy to svg in overview
    
    * change layout for item in list of icons
    
    * feat(website): fix type onCopySVG function

--- website/src/components/Overview.tsx
+++ website/src/components/Overview.tsx
@@ -4,6 +4,7 @@ import { ApiIcon } from "../types";
 
 import * as Bootstrap from "@chakra-icons/bootstrap";
 import * as Carbon from "@chakra-icons/carbon";
+import * as CryptoCurrency from "@chakra-icons/cryptocurrency-icons";
 import * as Feather from "@chakra-icons/feather";
 import * as FlatIcon from "@chakra-icons/flat-icon";
 import * as Ionicons from "@chakra-icons/ionicons";
@@ -12,87 +13,103 @@ import * as SimpleLineIcons from "@chakra-icons/simple-line-icons";
 import * as TablerIcons from "@chakra-icons/tabler-icons";
 import * as TypIcons from "@chakra-icons/typicons";
 import {
-  Button,
-  Code,
   ComponentWithAs,
+  HStack,
   IconProps,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
   SimpleGrid,
   Stack,
   StackProps,
   Text,
+  Tooltip,
+  useClipboard,
+  useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 
 const Item = ({ children, name, code, creator, ...props }: StackProps & ApiIcon) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const iconRef = React.useRef<HTMLElement & SVGElement>(null);
+  const { onCopy: _onCopySVG } = useClipboard(iconRef?.current?.outerHTML ?? "");
+  const { onCopy: _onCopyCode } = useClipboard(code);
+  const toast = useToast();
+
+  const onCopySVG = () => {
+    _onCopySVG();
+    toast({
+      title: "Copied SVG to clipboard.",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
+  const onCopyCode = () => {
+    _onCopyCode();
+    toast({
+      title: "Copied Import Code to clipboard.",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
   const color = "gray.300";
   const hoverColor = "blue.500";
-  const toast = useToast();
+
   return (
-    <Popover isLazy placement="top-start">
-      <PopoverTrigger>
-        <Stack
-          _hover={{
-            color: hoverColor,
-            fill: hoverColor,
-            borderStyle: "solid",
-            borderColor: hoverColor,
-          }}
-          alignItems="center"
-          aria-label={name}
-          borderColor={color}
-          borderRadius="lg"
-          borderStyle="dotted"
-          borderWidth="thin"
-          boxSize="24"
-          color={color}
-          cursor="pointer"
-          fill={color}
-          justifyContent="center"
-          {...props}
-        >
-          {React.isValidElement(children) ? React.cloneElement(children, { boxSize: "9" }) : null}
-          <Text fontSize="xs" fontWeight="bold" isTruncated maxW="full" px="2">
-            {name}
-          </Text>
-        </Stack>
-      </PopoverTrigger>
-      <PopoverContent width="auto">
-        <PopoverHeader fontWeight="bold">
-          {creator} / {name}
-        </PopoverHeader>
-        <PopoverArrow />
-        <PopoverCloseButton />
-        <PopoverBody>
-          <Code colorScheme="blackAlpha">{code}</Code>
-          <Button
-            ml={2}
-            onClick={() => {
-              navigator.clipboard
-                .writeText(code)
-                .then(() => {
-                  toast({
-                    title: "Copied to clipboard.",
-                    status: "success",
-                    duration: 3000,
-                    isClosable: true,
-                  });
-                })
-                .catch(console.error);
-            }}
-            size="xs"
-          >
-            Copy
-          </Button>
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+    <Stack
+      _hover={{
+        color: hoverColor,
+        fill: hoverColor,
+        borderStyle: "solid",
+        borderColor: hoverColor,
+      }}
+      alignItems="center"
+      aria-label={name}
+      borderColor={color}
+      borderRadius="lg"
+      borderStyle="dotted"
+      borderWidth="thin"
+      boxSize="24"
+      color={color}
+      fill={color}
+      justifyContent="center"
+      onMouseEnter={onOpen}
+      onMouseLeave={onClose}
+      {...props}
+    >
+      {React.isValidElement(children) ? React.cloneElement(children, { boxSize: "9", ref: iconRef }) : null}
+      {isOpen ? (
+        <HStack color="gray.900" fill="gray.900" isInline spacing="3">
+          <Tooltip hasArrow label="copy Import Code to clipboard." shouldWrapChildren>
+            <Carbon.Copy
+              _hover={{
+                color: hoverColor,
+                fill: hoverColor,
+              }}
+              cursor="pointer"
+              onClick={onCopyCode}
+            />
+          </Tooltip>
+          {iconRef.current ? (
+            <Tooltip label="copy SVG to clipboard." shouldWrapChildren>
+              <Bootstrap.FiletypeSvg
+                _hover={{
+                  color: hoverColor,
+                  fill: hoverColor,
+                }}
+                cursor="pointer"
+                onClick={onCopySVG}
+              />
+            </Tooltip>
+          ) : null}
+        </HStack>
+      ) : (
+        <Text fontSize="xs" fontWeight="bold" isTruncated maxW="full" px="2">
+          {name}
+        </Text>
+      )}
+    </Stack>
   );
 };
 
@@ -118,6 +135,8 @@ export function Overview({ icons }: { icons: ApiIcon[] }) {
               [String(_creator === "simple-line-icons")]: () =>
                 getKeyValue(SimpleLineIcons, name as keyof typeof SimpleLineIcons),
               [String(_creator === "ionicons")]: () => getKeyValue(Ionicons, name as keyof typeof Ionicons),
+              [String(_creator === "cryptocurrency-icons")]: () =>
+                getKeyValue(CryptoCurrency, name as keyof typeof CryptoCurrency),
             }.true());
 
           const Component = getComponent(iconName, creator.toLowerCase());
